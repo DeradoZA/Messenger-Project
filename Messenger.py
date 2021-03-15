b98d51a3f724c1f77e7c7717ba1aa1fe5c26e443
DeradoZA <danicoetzee1@gmail.com>
2021-03-15 15:51:53 +0200

    Created initial simple server and client setup

--- Messenger.py
+++ Messenger.py
@@ -1,2 +1,9 @@
+import socket
+
 if __name__ == "__main__":
-    print("Hello World")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1',55555))
+
+    message = s.recv(1024)
+
+    print(message.decode())
