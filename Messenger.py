7a1a5fd08693be64cd840757f22f9b999d112f13
DeradoZA <danicoetzee1@gmail.com>
2021-03-18 16:35:10 +0200

    Added multi-user functionality to the server

--- Messenger.py
+++ Messenger.py
@@ -1,9 +1,22 @@
-import socket
+import socket, sys
 
 if __name__ == "__main__":
+
+    HEADER = 10
+    PORT = 5050
+    SERVERIP = socket.gethostbyname(socket.gethostname())
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('127.0.0.1',55555))
+    s.connect((SERVERIP, PORT))
+    message = ""
 
-    message = s.recv(1024)
+    while True:
+        message = input()
 
-    print(message.decode())
+        if message == "!disconnect":
+            print("Thanks for connecting!")
+            s.close()
+            break
+        else:
+            s.send(message.encode())
+
+    
--- SimpleServer.py
+++ SimpleServer.py
@@ -1,13 +1,31 @@
-import socket
+import socket, threading, sys
+
+def start():
+    print("SERVER HAS STARTED")
+    while True:
+        client, address = s.accept()
+
+        thread = threading.Thread(target = Handle_Users, args = (client, address))
+        thread.start()
+
+def Handle_Users(client, address):
+    print("Connected to {}".format(address))
+    print("Total amount of users connected: {}".format(threading.active_count() - 1))
+    msg = ""
+
+    while True:
+        msg = client.recv(1024)
+        print(msg.decode())
+
 
 if __name__ == "__main__":
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('127.0.0.1', 55555))
-    s.listen()
+    HEADER = 10
+    PORT = 5050
+    SERVERIP = socket.gethostbyname(socket.gethostname())
 
-    while True:
-        client, address = s.accept()
-        print("Connected to {}".format(address))
-        client.send("You are connected!".encode())
-        client.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((SERVERIP, PORT))
+    s.listen()
+    start()
+
